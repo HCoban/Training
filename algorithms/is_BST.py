0d7555722de7981a327c433bcb6a55f3a008dd8d
Halil Coban <halil.coban@gmail.com>
2016-11-28 18:24:42 -0800

    fixed bst

--- algorithms/is_BST.py
+++ algorithms/is_BST.py
@@ -5,7 +5,7 @@ def check_binary_search_tree_(root):
 def is_BST(node, min=None, max=None):
   if node is None:
     return True
-  elif (min != None and node.data <= min) or (max != None and node.data >= max):
+  elif (min and node.data <= min) or (max and node.data >= max):
         return False
   else:
         return is_BST(node.left, min, node.data) and is_BST(node.right, node.data, max)
